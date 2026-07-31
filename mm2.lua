dfe4b56572a2faa93408657ce0957df81279363c
znex-loader <azuresp46@gmail.com>
2026-08-01 01:05:48 +0800

    Update script URL for mm2.lua

--- mm2.lua
+++ mm2.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/znex-loader/Extended/refs/heads/main/znex.mm2"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/znex-loader/Extended/refs/heads/main/obfuscated_t7ovw7TtI3MVpopARV5RRl7dSoiduIE4ITrggOQBXRAklzhTo74x8yF9Q19jyu16ykGcb.lua"))()
