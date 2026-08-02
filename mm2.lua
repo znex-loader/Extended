76ad1de40d158e4d004eac2238bee5cd39ede845
znex-loader <azuresp46@gmail.com>
2026-08-02 12:09:17 +0800

    Update mm2.lua

--- mm2.lua
+++ mm2.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ZnexDev/Loader/refs/heads/main/mm2.luac.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ZnexDev/Loader/refs/heads/main/obfuscated_uGv17JbIy67bdmlCGwau1KwISNS5tCYV9e1cu9eXYT0mChHX2ipULtdw9vNowk8GdJ5CQ.lua"))()
